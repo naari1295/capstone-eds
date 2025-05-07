bc08bd47bbb4829408ec01f16f58164e47f8fe36
nari1295 <narayanammamaguluri@gmail.com>
2025-05-07 18:11:06 +0530

    first commit

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,6 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
-
+ 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -10,11 +10,41 @@ export default async function decorate(block) {
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
-
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
-  block.append(footer);
+ 
+  console.log(block);
+ 
+  block.textContent='';
+  const footerDivContainer=document.createElement('div');
+  const footerSocialDiv = document.createElement('div');
+  const footerSocialTextDiv = document.createElement('div');
+  const footerSocialIconsDiv = document.createElement('div');
+  const footerDivSpace=document.createElement('div');
+  footerDivSpace.classList.add('blank-space');
+  const footerClasses=['footer-nav','footer-text'];
+  let index=0;
+  while(fragment.firstElementChild){
+    fragment.firstElementChild.classList.add(footerClasses[index]);
+    footerDivContainer.append(fragment.firstElementChild)
+    if(index==0){
+      footerDivContainer.append(footerDivSpace);
+    }
+    index+=1;
+  }
+ 
+  const socials = footerDivContainer.querySelector('.default-content-wrapper p:last-child');
+  const socialIcons = socials.querySelectorAll('a');
+ 
+  footerSocialTextDiv.textContent = socials.textContent;
+  socialIcons.forEach((icon) => {
+    footerSocialIconsDiv.append(icon);
+  })
+ 
+  footerSocialDiv.append(footerSocialTextDiv);
+  footerSocialDiv.append(footerSocialIconsDiv);
+ 
+  socials.remove();
+ 
+  footerDivContainer.querySelector('.default-content-wrapper').append(footerSocialDiv);
+  block.append(footerDivContainer);
 }
+ 
